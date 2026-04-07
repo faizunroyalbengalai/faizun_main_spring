930958346d3c2943b2cf464eba938ac1cca95527
faizunroyalbengalai <faizun.royalbengalai@gmail.com>
2026-04-07 18:57:56 +0600

    chore: bootstrap Spring Boot starter from Spring Initializr

---
+++ src/main/java/com/example/faizunmainspring/FaizunMainSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.faizunmainspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaizunMainSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaizunMainSpringApplication.class, args);
+	}
+
+}
